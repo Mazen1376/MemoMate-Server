0092deae3507adbdd177629351fe071c6db4d581
Mazen_Yasser.com <Mazen1376797@gmail.com>
2026-02-18 16:44:21 +0200

    syntax modification

--- src/lib/mongoDB.ts
+++ src/lib/mongoDB.ts
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGO_URI || process.env.MONGODB_URI;
+    const mongoUri = process.env.MONGO_URI;
     if (!mongoUri) {
       throw new Error("MongoDB URI is missing in .env");
     }
-    const conn = await mongoose.connect(mongoUri);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(mongoUri);
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`Error connection to MongoDB: ${(error as Error).message}`);
   }
