65f34a6be4eae0a9412b7c5aceb84bcdd4bf7ee0
Mazen_Yasser.com <Mazen1376797@gmail.com>
2026-02-18 18:21:38 +0200

    login function

--- src/routes/doctorRoutes.ts
+++ src/routes/doctorRoutes.ts
@@ -5,6 +5,7 @@ import {
   getDoctorById,
   updateDoctor,
   deleteDoctor,
+  doctorLogin,
 } from "../controllers/doctorController.js";
 
 const doctorRoutes = Router();
@@ -12,6 +13,7 @@ const doctorRoutes = Router();
 doctorRoutes.get("/api/doctor", getDoctors);         /////////////// doctors
 doctorRoutes.get("/api/doctor/:id", getDoctorById);
 doctorRoutes.post("/api/doctor", createDoctor);
+doctorRoutes.post("/api/doctor/login", doctorLogin);
 doctorRoutes.put("/api/doctor/:id", updateDoctor);
 doctorRoutes.delete("/api/doctor/:id", deleteDoctor);
 
